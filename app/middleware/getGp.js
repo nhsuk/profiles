14a909cffe25aaf8935e71ac07be17f26142a94b
steven hunt <st3v3n.hunt@gmail.com>
2017-02-20 10:24:30 +0000

    :two_men_holding_hands: Use promises rather than generators

--- app/middleware/getGp.js
+++ app/middleware/getGp.js
@@ -1,27 +1,26 @@
 const MongoClient = require('mongodb').MongoClient;
-const co = require('co');
 const log = require('../lib/logger');
 const config = require('../../config/config').mongodb;
 
 function getGp(req, res, next) {
   const connectionString = config.connectionString;
 
-  co(function* () {
-    const db = yield MongoClient.connect(connectionString);
+  MongoClient.connect(connectionString).then((db) => {
     log.info(`Connected to ${connectionString}`);
 
     const collection = db.collection(config.collection);
 
     const choicesId = parseInt(res.locals.choicesId, 10);
-    const gp = yield collection.findOne({ choicesId });
 
-    log.debug({ gp }, `Returned when searching for ${choicesId}`);
-    // eslint-disable-next-line no-param-reassign
-    res.locals.gp = gp;
+    collection.findOne({ choicesId }).then((gp) => {
+      log.debug({ gp }, `Returned when searching for ${choicesId}`);
+      // eslint-disable-next-line no-param-reassign
+      res.locals.gp = gp;
 
-    db.close();
+      db.close();
 
-    next();
+      next();
+    });
   }).catch((err) => {
     log.error(err.stack, `Error connecting to ${connectionString}`);
     next(err);
