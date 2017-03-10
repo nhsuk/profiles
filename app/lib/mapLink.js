73dac374ef362c6830f7b8101026ab579e7202e6
ajthomascouk <alex.thomas@nhs.net>
2017-03-10 11:29:10 +0000

    :innocent: fix travis error

--- app/lib/mapLink.js
+++ app/lib/mapLink.js
@@ -1,9 +1,9 @@
 function addUrl(gpName) {
-    const name = gpName.name;
-    const address = gpName.address.addressLines;
-    const postcode = gpName.address.postcode;
-    const url = `http://maps.google.com/?q=${name},${address},${postcode}`.replace(/ /g, '+');
-    return url;
+  const name = gpName.name;
+  const address = gpName.address.addressLines;
+  const postcode = gpName.address.postcode;
+  const url = `http://maps.google.com/?q=${name},${address},${postcode}`.replace(/ /g, '+');
+  return url;
 }
 
 module.exports = {
