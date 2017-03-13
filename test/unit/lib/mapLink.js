22180560a579e698300dfa314b0ff129d0566717
ajthomascouk <alex.thomas@nhs.net>
2017-03-13 14:17:12 +0000

    :muscle: fix travis error

--- test/unit/lib/mapLink.js
+++ test/unit/lib/mapLink.js
@@ -6,11 +6,11 @@ const expect = chai.expect;
 describe('mapLink', () => {
   it('returns a fully qualified google map url', () => {
     const gp = {};
-    gp.name = `Gilberdyke Health Centre`;
+    gp.name = 'Gilberdyke Health Centre';
     gp.address = {};
-    gp.address.addressLines = `The Health Centre,Thornton Dam Lane,Gilberdyke,Gilberdyke`;
-    gp.address.postcode = `HU15 2UL`;
-    const expectedResult = `https://maps.google.com/?q=Gilberdyke+Health+Centre,The+Health+Centre,Thornton+Dam+Lane,Gilberdyke,Gilberdyke,HU15+2UL`;
+    gp.address.addressLines = 'The Health Centre,Thornton Dam Lane,Gilberdyke,Gilberdyke';
+    gp.address.postcode = 'HU15 2UL';
+    const expectedResult = 'https://maps.google.com/?q=Gilberdyke+Health+Centre,The+Health+Centre,Thornton+Dam+Lane,Gilberdyke,Gilberdyke,HU15+2UL';
     const result = mapLink.generateUrl(gp);
 
     expect(result).to.be.equal(expectedResult);
