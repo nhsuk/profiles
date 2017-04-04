e1a1bf4373fb4a736c2db7d3ffa5cd140a356906
ajthomascouk <alex.thomas@nhs.net>
2017-04-04 12:41:49 +0100

    :yum: optimising custom analytics code

--- public/js/custom-analytics.js
+++ public/js/custom-analytics.js
@@ -1,7 +1,7 @@
 jQuery(function($) {
 
   // Debug flag
-  var debugMode = false;
+  var debugMode = true;
 
   // Default time delay before checking location
   var callBackTime = 100;
@@ -27,12 +27,27 @@ jQuery(function($) {
   // Get some information about the current page
   var pageTitle = document.title;
 
-  // Track the aticle load
-  if (!debugMode) {
-      //ga('send', 'event', 'Reading', 'ArticleLoaded', '');
+  function scrolledSection(start, section) {
+    currentTime = new Date();
+    if (start) {
+      scrollStart = currentTime.getTime();
+      timeToScroll = Math.round((scrollStart - beginning) / 1000);
+      if (!debugMode) {
+          ga('send', 'event', section, timeToScroll+'s');
+      } else {
+          alert(section + ' (Time: ' + timeToScroll + 's)');
+      }
 
-  } else {
-      console.log('Start custom analytics');
+    } else {
+      scrollEnd = currentTime.getTime();
+      timeToScroll = Math.round((scrollEnd - scrollStart) / 1000);
+      if (!debugMode) {
+          ga('send', 'event', 'User reached the "' + section +'" section', timeToScroll+'s');
+      } else {
+          alert('User reached the "' + section + '" section (Time: ' + timeToScroll + 's)');
+      }
+    }
+    scroller = true;
   }
 
   // Check the location and track user
@@ -47,93 +62,38 @@ jQuery(function($) {
 
       // If user starts to scroll send an event
       if (bottom > readerLocation && !scroller) {
-          currentTime = new Date();
-          scrollStart = currentTime.getTime();
-          timeToScroll = Math.round((scrollStart - beginning) / 1000);
-          if (!debugMode) {
-              console.log('started reading ' + timeToScroll);
-              ga('send', 'event', 'User started scrolling', timeToScroll+'s');
-          } else {
-              alert('User started scrolling (Time: ' + timeToScroll + 's)');
-          }
-          scroller = true;
+        scrolledSection(true, 'User started scrolling');
+        scroller = true;
       }
 
-      if (bottom > $('h2.opening-times:contains("GP")').offset().top + $('h2.opening-times:contains("GP")').outerHeight() && !endOpeningTimes) {
-          currentTime = new Date();
-          openingTimesScrollEnd = currentTime.getTime();
-          timeToOpeningTimesEnd = Math.round((openingTimesScrollEnd - scrollStart) / 1000);
-          if (!debugMode) {
-              ga('send', 'event', 'User reached the "GP Opening Times" section', timeToOpeningTimesEnd+'s');
-          } else {
-              alert('User reached the "GP Opening Times" section (Time: ' + timeToOpeningTimesEnd + 's)');
-          }
-          endOpeningTimes = true;
+      if ($('h2.opening-times:contains("GP")').length && bottom > $('h2.opening-times:contains("GP")').offset().top + $('h2.opening-times:contains("GP")').outerHeight() && !endOpeningTimes) {
+        scrolledSection(false, 'GP Opening Times');
+        endOpeningTimes = true;
       }
 
-      if (bottom > $('h2:contains("What patients say about this surgery")').offset().top + $('h2:contains("What patients say about this surgery")').outerHeight() && !endPatientsSay) {
-          currentTime = new Date();
-          patientsSayScrollEnd = currentTime.getTime();
-          timeToPatientsSayEnd = Math.round((patientsSayScrollEnd - scrollStart) / 1000);
-          if (!debugMode) {
-              ga('send', 'event', 'User reached the "What patients say about this surgery" section', timeToPatientsSayEnd+'s');
-          } else {
-              alert('User reached the "What patients say about this surgery" section (Time: ' + timeToPatientsSayEnd + 's)');
-          }
-          endPatientsSay = true;
+      if ($('h2:contains("What patients say about this surgery")').length && bottom > $('h2:contains("What patients say about this surgery")').offset().top + $('h2:contains("What patients say about this surgery")').outerHeight() && !endPatientsSay) {
+        scrolledSection(false, 'What patients say about this surgery');
+        endPatientsSay = true;
       }
 
-      if (bottom > $('h2:contains("GPs at this surgery")').offset().top + $('h2:contains("GPs at this surgery")').outerHeight() && !endSurgeryGPs) {
-          currentTime = new Date();
-          surgeryGPsScrollEnd = currentTime.getTime();
-          timeToSurgeryGPsEnd = Math.round((surgeryGPsScrollEnd - scrollStart) / 1000);
-          if (!debugMode) {
-              ga('send', 'event', 'User reached the "GPs at this surgery" section', timeToSurgeryGPsEnd+'s');
-          } else {
-              alert('User reached the "GPs at this surgery" section (Time: ' + timeToSurgeryGPsEnd + 's)');
-          }
-          endSurgeryGPs = true;
+      if ($('h2:contains("GPs at this surgery")').length && bottom > $('h2:contains("GPs at this surgery")').offset().top + $('h2:contains("GPs at this surgery")').outerHeight() && !endSurgeryGPs) {
+        scrolledSection(false, 'GPs at this surgery');
+        endSurgeryGPs = true;
       }
 
-      if (bottom > $('h2:contains("Services at this surgery")').offset().top + $('h2:contains("Services at this surgery")').outerHeight() && !endServices) {
-          currentTime = new Date();
-          servicesScrollEnd = currentTime.getTime();
-          timeToServicesEnd = Math.round((servicesScrollEnd - scrollStart) / 1000);
-          if (!debugMode) {
-              ga('send', 'event', 'User reached the "Services at this surgery" section', timeToServicesEnd+'s');
-          } else {
-              alert('User reached the "Services at this surgery" section (Time: ' + timeToServicesEnd + 's)');
-          }
-          endServices = true;
+      if ($('h2:contains("Services at this surgery")').length && bottom > $('h2:contains("Services at this surgery")').offset().top + $('h2:contains("Services at this surgery")').outerHeight() && !endServices) {
+        scrolledSection(false, 'Services at this surgery');
+        endServices = true;
       }
 
-      if (bottom > $('h2:contains("Parking")').offset().top + $('h2:contains("Parking")').outerHeight() && !endParking) {
-          currentTime = new Date();
-          parkingScrollEnd = currentTime.getTime();
-          timeToParkingEnd = Math.round((parkingScrollEnd - scrollStart) / 1000);
-          if (!debugMode) {
-              ga('send', 'event', 'User reached the "Parking and accessibility" section', timeToParkingEnd+'s');
-          } else {
-              alert('User reached the "Parking and accessibility" section (Time: ' + timeToParkingEnd + 's)');
-          }
-          endParking = true;
+      if ($('h2:contains("Parking")').length && bottom > $('h2:contains("Parking")').offset().top + $('h2:contains("Parking")').outerHeight() && !endParking) {
+        scrolledSection(false, 'Parking and accessibility');
+        endParking = true;
       }
 
       if (bottom > $('footer ul.link-list').offset().top + $('footer ul.link-list').outerHeight() && !didComplete) {
-          currentTime = new Date();
-          end = currentTime.getTime();
-          totalTime = Math.round((end - scrollStart) / 1000);
-          if (!debugMode) {
-              if (totalTime < 20) {
-                  _gaq.push(['_setCustomVar', 5, 'ReaderType', 'Scanner', 2]);
-              } else {
-                  _gaq.push(['_setCustomVar', 5, 'ReaderType', 'Reader', 2]);
-              }
-              ga('send', 'event', 'User reached the end of the page', totalTime+'s');
-          } else {
-              alert('User reached the end of the page (Time: ' + totalTime + 's)');
-          }
-          didComplete = true;
+        scrolledSection(false, 'end of the page');
+        didComplete = true;
       }
   }
 
