e35c8c6672fb5c2508885d60a68a5dcb77edff75
Steven Hunt <st3v3n.hunt@gmail.com>
2017-04-11 11:27:46 +0100

    :bar_chart: Add WebTrends custom events

---
+++ public/js/custom-wt.js
@@ -0,0 +1,19 @@
+jQuery(function($) {
+  $(document).ready(function () {
+    $('.gp-email').on('click', function () {
+      Webtrends.multiTrack({argsa: ['DCSext.CTSLinkClicks', 'GP-Email', 'WT.dl', '121']});
+    });
+    $('.gp-book-online').on('click', function () {
+      Webtrends.multiTrack({argsa: ['DCSext.CTSLinkClicks', 'GP-Booking', 'WT.dl', '121']});
+    });
+    $('.gp-patient-survey').on('click', function () {
+      Webtrends.multiTrack({argsa: ['DCSext.CTSLinkClicks', 'GP-Patient_Survey', 'WT.dl', '121']});
+    });
+    $('.gp-website').on('click', function () {
+      Webtrends.multiTrack({argsa: ['DCSext.CTSLinkClicks', 'GP-Website', 'WT.dl', '121']});
+    });
+    $('.gp-website-opening-times').on('click', function () {
+      Webtrends.multiTrack({argsa: ['DCSext.CTSLinkClicks', 'GP-Website_OpeningTimes', 'WT.dl', '121']});
+    });
+  });
+});
