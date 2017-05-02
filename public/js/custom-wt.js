3d7f52be952e74a9c1493200461acfac22afb2fc
ajthomascouk <alex.thomas@nhs.net>
2017-05-02 08:50:02 +0100

    :expressionless: Add WT tracking to register links

--- public/js/custom-wt.js
+++ public/js/custom-wt.js
@@ -1,24 +1,30 @@
 jQuery(function($) {
   $(document).ready(function () {
-    $('.gp-email').on('click', function () {
+    $('.gp-email').on('touchstart click', function () {
       Webtrends.multiTrack({argsa: ['DCSext.CTSLinkClicks', 'GP-Email', 'WT.dl', '121']});
     });
-    $('.gp-book-online').on('click', function () {
+    $('.gp-book-online').on('touchstart click', function () {
       Webtrends.multiTrack({argsa: ['DCSext.CTSLinkClicks', 'GP-Booking', 'WT.dl', '121']});
     });
-    $('.repeat-prescription-online').on('click', function () {
+    $('.repeat-prescription-online').on('touchstart click', function () {
       Webtrends.multiTrack({argsa: ['DCSext.CTSLinkClicks', 'GP-Repeat-Prescription', 'WT.dl', '121']});
     });
-    $('.coded-records-online').on('click', function () {
+    $('.coded-records-online').on('touchstart click', function () {
       Webtrends.multiTrack({argsa: ['DCSext.CTSLinkClicks', 'GP-Medical-Record', 'WT.dl', '121']});
     });
-    $('.gp-patient-survey').on('click', function () {
+    $('.gp-patient-survey').on('touchstart click', function () {
       Webtrends.multiTrack({argsa: ['DCSext.CTSLinkClicks', 'GP-Patient_Survey', 'WT.dl', '121']});
     });
-    $('.gp-website').on('click', function () {
+    $('.gp-website-register').on('touchstart click', function () {
+      Webtrends.multiTrack({argsa: ['DCSext.CTSLinkClicks', 'GP-Register_Patient', 'WT.dl', '121']});
+    });
+    $('.gp-website-register-other').on('touchstart click', function () {
+      Webtrends.multiTrack({argsa: ['DCSext.CTSLinkClicks', 'GP-Register_Other', 'WT.dl', '121']});
+    });
+    $('.gp-website').on('touchstart click', function () {
       Webtrends.multiTrack({argsa: ['DCSext.CTSLinkClicks', 'GP-Website', 'WT.dl', '121']});
     });
-    $('.gp-website-opening-times').on('click', function () {
+    $('.gp-website-opening-times').on('touchstart click', function () {
       Webtrends.multiTrack({argsa: ['DCSext.CTSLinkClicks', 'GP-Website_OpeningTimes', 'WT.dl', '121']});
     });
   });
