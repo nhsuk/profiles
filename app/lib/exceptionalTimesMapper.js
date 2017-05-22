fc632e01c8593a48e9b75b36452fe014b3132f7f
Eva Barabas <b_seven_e@yahoo.co.uk>
2017-05-22 12:27:24 +0100

    :clock1: exceptional opening times, tests pending

---
+++ app/lib/exceptionalTimesMapper.js
@@ -0,0 +1,84 @@
+const timeUtils = require('./timeUtils');
+
+const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+function joinContiguousTimes(date) {
+  const fixedDates = date.reduce((o, session) => {
+    /* eslint-disable no-param-reassign */
+    if (o.prev && session.opens === o.prev.closes) {
+      o.prev.closes = session.closes;
+    } else {
+      o.list.push(session);
+      o.prev = session;
+    }
+    /* eslint-enable no-param-reassign */
+    return o;
+  }, { list: [], prev: undefined });
+
+  return fixedDates.list;
+}
+
+function mapDate(date) {
+  // eslint-disable-next-line arrow-body-style
+  const sessions = joinContiguousTimes(date).map((session) => {
+    return `${timeUtils.toAmPm(session.opens)} to ${timeUtils.toAmPm(session.closes)}`;
+  });
+
+  if (sessions.length === 0) {
+    sessions.push('Closed');
+  }
+  return sessions;
+}
+
+function getDates(times) {
+  const filteredTimes = {};
+  Object.keys(times).forEach((exceptionalTime) => {
+    if (timeUtils.isDateInWindow(exceptionalTime, Date.now(), 14) === true) {
+      filteredTimes[exceptionalTime] = times[exceptionalTime];
+    }
+  });
+  return filteredTimes;
+}
+
+function addPadding(parsedTimes) {
+  const counts = parsedTimes.map(time => time.sessions.length);
+  const max = Math.max(...counts);
+
+  parsedTimes.forEach((time) => {
+    if (time.sessions.length < max) {
+      // eslint-disable-next-line no-param-reassign
+      time.padding = (max - time.sessions.length);
+    }
+  });
+
+  return parsedTimes;
+}
+
+function mapDates(times) {
+  const parsedTimes = [];
+
+  Object.keys(getDates(times)).forEach((date) => {
+    const formattedDate = timeUtils.toReadableDate(date, daysOfWeek, months);
+    const sessions = mapDate(times[date]);
+    parsedTimes.push({ formattedDate, sessions });
+  });
+  return addPadding(parsedTimes);
+}
+
+function timesValid(allTimes) {
+  // the source data may not have alterations even if opening times exist
+  return allTimes && allTimes.alterations;
+}
+
+function mapAll(allTimes) {
+  return timesValid(allTimes) ? {
+    alterations: mapDates(allTimes.alterations),
+  } : {};
+}
+
+module.exports = {
+  mapAll,
+  mapDates,
+  mapDate,
+};
--- app/lib/timeUtils.js
+++ app/lib/timeUtils.js
@@ -19,6 +19,26 @@ function toAmPm(timeString) {
   return `${hours}${mins}${suffix}`;
 }
 
+function dayDiff(secondDate, firstDate) {
+  return Math.trunc((secondDate - firstDate) / (1000 * 60 * 60 * 24));
+}
+
+function isDateInWindow(dateString, now, noDays) {
+  const exceptionalDate = new Date(dateString);
+  if ((dayDiff(exceptionalDate, new Date(now)) <= noDays) &&
+    (dayDiff(exceptionalDate, new Date(now)) >= 0)) {
+    return true;
+  }
+  return false;
+}
+
+function toReadableDate(dateString, days, months) {
+  const exceptionalDate = new Date(dateString);
+  return `${days[exceptionalDate.getDay()]} ${exceptionalDate.getDate()} ${months[exceptionalDate.getMonth()]}`;
+}
+
 module.exports = {
-  toAmPm
+  toAmPm,
+  toReadableDate,
+  isDateInWindow,
 };
