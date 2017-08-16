637b4cc50bf98a49eb8eed9da7cea77291875513
steven hunt <st3v3n.hunt@gmail.com>
2017-08-16 08:39:56 +0100

    :hammer: Encode maps URL

--- test/unit/lib/mapLink.js
+++ test/unit/lib/mapLink.js
@@ -1,17 +1,21 @@
+const qs = require('querystring');
 const chai = require('chai');
 const mapLink = require('../../../app/lib/mapLink.js');
 
 const expect = chai.expect;
 
 describe('mapLink', () => {
-  it('returns a fully qualified google map url', () => {
+  it('returns a fully qualified, URI encoded google map url', () => {
     const gp = {};
-
     gp.name = 'Gilberdyke Health Centre';
     gp.address = {};
-    gp.address.addressLines = 'The Health Centre,Thornton Dam Lane,Gilberdyke,Gilberdyke';
+    gp.address.addressLines = 'The Health & Centre,Thornton Dam Lane,Gilberdyke,Gilberdyke';
     gp.address.postcode = 'HU15 2UL';
-    const expectedResult = 'https://maps.google.com/?q=Gilberdyke+Health+Centre,The+Health+Centre,Thornton+Dam+Lane,Gilberdyke,Gilberdyke,HU15+2UL';
+
+    const expectedQuery = `${gp.name},${gp.address.addressLines},${gp.address.postcode}`;
+    const encodedQuery = qs.escape(expectedQuery);
+    const expectedResult = `https://maps.google.com/?q=${encodedQuery}`;
+
     const result = mapLink.generateUrl(gp);
 
     expect(result).to.be.equal(expectedResult);
