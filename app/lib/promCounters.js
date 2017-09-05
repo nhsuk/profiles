43dba9b2edf2218203322f0ef405551b1721e294
Steven Hunt <st3v3n.hunt@gmail.com>
2017-09-05 11:53:15 +0100

    :hospital: Instrument GP Profile views

--- app/lib/promCounters.js
+++ app/lib/promCounters.js
@@ -3,4 +3,5 @@ const promClient = require('./promBundle').promClient;
 module.exports = {
   applicationStarts: new promClient.Counter({ name: 'app_start', help: 'times the application has been started' }),
   notFound: new promClient.Counter({ name: 'not_found', help: 'not found page has been returned' }),
+  gpProfileRender: new promClient.Counter({ name: 'gp_profile', help: 'GP profile has been returned' }),
 };
--- app/middleware/renderer.js
+++ app/middleware/renderer.js
@@ -1,16 +1,17 @@
 const log = require('../lib/logger');
 const mapLink = require('../lib/mapLink');
-const notFoundCounter = require('../lib/promCounters').notFound;
+const counters = require('../lib/promCounters');
 
 function notFound(req, res) {
   log.warn({ req }, 'Not found (404).');
-  notFoundCounter.inc(1);
+  counters.notFoundCounter.inc(1);
   res.status(404);
   res.render('404');
 }
 
 function gpSurgeries(req, res) {
   if (res.locals.gp) {
+    counters.gpProfileRender.inc(1);
     res.render('hub', {
       mapUrl: mapLink.generateUrl(res.locals.gp),
     });
