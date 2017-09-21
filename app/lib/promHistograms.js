0c3834798a100d1e66e87066e754d240cd6fbb30
mark <markharrop@yahoo.com>
2017-09-21 14:49:42 +0100

    :card_index: add lookup by choicesId to esClient

--- app/lib/promHistograms.js
+++ app/lib/promHistograms.js
@@ -3,4 +3,5 @@ const buckets = require('./constants').promHistogramBuckets;
 
 module.exports = {
   esGetGP: new promClient.Histogram({ name: 'es_get_gp', help: 'Duration histogram of Elasticsearch request to get a GP', buckets }),
+  esGetGPByOdsCode: new promClient.Histogram({ name: 'es_get_gp_by_ods_code', help: 'Duration histogram of Elasticsearch request to get a GP by ODS code', buckets }),
 };
